9fa3e374fd1d78dd8dff632c7e879280246424e9
Samarth64s <115937579+Samarth64s@users.noreply.github.com>
2024-03-02 16:19:54 +0530

    layout overflow

--- hiring_employer/src/pages/Layout.js
+++ hiring_employer/src/pages/Layout.js
@@ -4,13 +4,13 @@ import Sidebar from "./../components/Sidebar";
 const Layout = ({ children }) => {
   return (
     <>
-      <div className="flex h-screen">
-        <div className="flex w-1/6 p-4 bg-black">
+      <div className="flex h-screen overflow-hidden">
+        <div className="flex w-1/6 p-4 bg-black overflow-y-auto">
           <Sidebar />
         </div>
-        <div className="flex-1 flex flex-col bg-slate-100">
+        <div className="flex-1 flex flex-col bg-slate-100 overflow-hidden">
           <Header />
-          <main>{children}</main>
+          <main className="flex-1 overflow-y-auto">{children}</main>
           {/* <Footer /> */}
         </div>
       </div>
